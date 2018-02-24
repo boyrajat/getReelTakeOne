fd401544cd7d1e714d234fdd02070719acb62790
Rajat Narula <boyrajat@hotmail.com>
2018-02-24 14:31:42 -0500

    Working OMDB Api and database

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -1,7 +1,8 @@
 // Import NPM dependency and Express router function.
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
+//var omdb = require('omdb');
 // Import data model.
 var db = require('../models');
 
@@ -24,13 +25,58 @@ router.get('/', function(req, res) {
 // POST route which calls Sequelize's create method with the movie name given.
 router.post('/api/new/movie', function(req, res) {
     var movieName = req.body.name;
+    // movieName = movieName.toLowerCase();
+    var queryUrl = "http://omdbapi.com/?apikey=40e9cece&t=" + movieName;
+
+    request(queryUrl, function(error, response, body) {
+        // console.log(error);
+        // console.log(response);
 
 
-    db.Movie.create({
-        movie_name: movieName
-    }).then(function() {
-        res.redirect('/');
+        if (!error && JSON.parse(body).Response !== 'False') {
+            console.log(JSON.parse(body));
+            console.log(JSON.parse(body).Poster);
+            // var reqData = '\n' + '<---- Here It Goes ----> ' +
+            //     '\n' + '> Title: ' + JSON.parse(body).Title +
+            //     '\n' + '> Year Released: ' + JSON.parse(body).Year +
+            //     '\n' + '> IMDB Rating: ' + JSON.parse(body).Ratings[0].Value +
+            //     '\n' + '> Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value +
+            //     '\n' + '> Countries: ' + JSON.parse(body).Country +
+            //     '\n' + '> Available in: ' + JSON.parse(body).Language +
+            //     '\n' + '> Starring: ' + JSON.parse(body).Actors +
+            //     '\n' + '> Here is a gist of it: ' + JSON.parse(body).Plot + '\n';
+
+            // console.log(reqData);
+
+            //                     fs.appendFile('./log.txt', '\n***MOVIE INFO LOGGED' + reqData + '\n', function() {
+            // console.log('\nMOVIE INFO HAS BEEN LOGGED TO log.txt FILE' + '\n');
+            // });
+
+
+
+        } else {
+            console.log("\nOops...something went wrong with you movie search. Try again...");
+            // fs.appendFile('./log.txt', '\n***MOVIE SEARCH ERROR LOGGED ' + JSON.parse(body).Error + '\n', function() {
+            // console.log('\nMOVIE SEARCH ERROR HAS BEEN LOGGED TO log.txt FILE' + '\n');
+        }
+        db.Movie.create({
+            movie_name: JSON.parse(body).Title,
+            movie_poster: JSON.parse(body).Poster,
+            movie_genre: JSON.parse(body).Genre,
+            movie_time: JSON.parse(body).Runtime,
+            movie_plot: JSON.parse(body).Plot,
+            movie_director: JSON.parse(body).Director,
+            movie_actors: JSON.parse(body).Actors,
+            movie_year: JSON.parse(body).Year,
+            movie_ratingImdb: JSON.parse(body).Ratings[0].Value,
+            movie_ratingRotten: JSON.parse(body).Ratings[1].Value
+        }).then(function() {
+            res.redirect('/');
+        });
     });
+
+
+
 });
 
 // POST route which calls Sequelize's create method with a customer name,
